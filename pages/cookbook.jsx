ea6834d10a6af04cbd2439ab541b71593d57f3fe
Shoujian Zheng <zheng.shoujian@outlook.com>
2024-04-20 01:29:58 +0800

    fix: restore check state in filter tag

--- pages/cookbook.jsx
+++ pages/cookbook.jsx
@@ -5,27 +5,27 @@ import { getPagesUnderRoute } from "nextra/context";
 import { Tags } from "components/tag";
 import { ExampleCard } from "components/example-card";
 
-const title = {
+const titleLocaleMap = {
   "zh-CN": "案例",
   en: "Cookbook",
 };
 
-const description = {
+const descLocaleMap = {
   "zh-CN": "使用 Pluto 构建的应用案例，提供端到端的实践指导。",
   en: "Examples for building applications with Pluto, emphasizing practical and comprehensive use cases.",
 };
 
-const programmingLang = {
+const langTagLocaleMap = {
   "zh-CN": "语言",
   en: "Language",
 };
 
-const platform = {
+const platTagLocaleMap = {
   "zh-CN": "平台",
   en: "Platform",
 };
 
-const other = {
+const otherTagLabelLocaleMap = {
   "zh-CN": "其他",
   en: "Other",
 };
@@ -36,28 +36,17 @@ function CookbookHeader() {
     <div className="max-w-screen-lg mx-auto pt-4 pb-8 mb-16 border-b border-gray-400 border-opacity-20">
       <h1>
         <span className="font-bold leading-tight lg:text-5xl">
-          {title[locale]}
+          {titleLocaleMap[locale]}
         </span>
       </h1>
       <p className="text-center text-gray-500 dark:text-gray-400 font-space-grotesk">
-        {description[locale]}
+        {descLocaleMap[locale]}
       </p>
     </div>
   );
 }
 
 function CookbookTags({ setSelectedTags, selectedTags }) {
-  function TagBlock({ label, tags, onClick }) {
-    return (
-      <tr>
-        <td className="font-bold pr-2">{label}:</td>
-        <td>
-          <Tags tags={tags} onClick={onClick} />
-        </td>
-      </tr>
-    );
-  }
-
   const pages = getPagesUnderRoute("/cookbook");
   const tags = pages.map((page) => flatTags(page.frontMatter?.tags)).flat();
   const uniqueTags = [];
@@ -65,6 +54,15 @@ function CookbookTags({ setSelectedTags, selectedTags }) {
     if (!uniqueTags.some((t) => t.toLowerCase() === tag.toLowerCase())) {
       uniqueTags.push(tag);
     }
+  });
+
+  const langTags = ["TypeScript", "Python"];
+  const platformTags = ["AWS", "Kubernetes", "AliCloud"];
+  const otherTags = uniqueTags.filter((tag) => {
+    return (
+      langTags.every((t) => t.toLowerCase() !== tag.toLowerCase()) &&
+      platformTags.every((t) => t.toLowerCase() !== tag.toLowerCase())
+    );
   });
 
   const handleTagClick = (tag) => {
@@ -75,35 +73,31 @@ function CookbookTags({ setSelectedTags, selectedTags }) {
     }
   };
 
-  const langTags = ["TypeScript", "Python"];
-  const platformTags = ["AWS", "Kubernetes", "AliCloud"];
-  const others = uniqueTags.filter((tag) => {
-    return (
-      langTags.every((t) => t.toLowerCase() !== tag.toLowerCase()) &&
-      platformTags.every((t) => t.toLowerCase() !== tag.toLowerCase())
-    );
-  });
-
   const { locale } = useRouter();
   return (
     <>
       <table className="block">
         <tbody>
-          <TagBlock
-            label={programmingLang[locale]}
-            tags={langTags}
-            onClick={handleTagClick}
-          />
-          <TagBlock
-            label={platform[locale]}
-            tags={platformTags}
-            onClick={handleTagClick}
-          />
-          <TagBlock
-            label={other[locale]}
-            tags={others}
-            onClick={handleTagClick}
-          />
+          <tr>
+            <td className="font-bold pr-2">{langTagLocaleMap[locale]}:</td>
+            <td>
+              <Tags tags={langTags} onClick={handleTagClick} />
+            </td>
+          </tr>
+          <tr>
+            <td className="font-bold pr-2">{platTagLocaleMap[locale]}:</td>
+            <td>
+              <Tags tags={platformTags} onClick={handleTagClick} />
+            </td>
+          </tr>
+          <tr>
+            <td className="font-bold pr-2">
+              {otherTagLabelLocaleMap[locale]}:
+            </td>
+            <td>
+              <Tags tags={otherTags} onClick={handleTagClick} />
+            </td>
+          </tr>
         </tbody>
       </table>
     </>
